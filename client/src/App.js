a24057ec4591c502807fdb55632d5fdcb836f88d
RyanAsh93 <blueheadryan93@yahoo.com>
2020-06-22 16:37:39 -0600

    register/login works

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react'
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import NavBar from './components/NavBar';
+import NoMatch from './components/NoMatch'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+  <Fragment>
+    <NavBar />
+    <Container>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </Switch>
+    </Container>
+  </Fragment>
+)
+
 
 export default App;
